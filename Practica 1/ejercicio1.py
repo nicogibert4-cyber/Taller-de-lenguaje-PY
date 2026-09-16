44d8387218fb10d610859c615fef5e3798a41c4d
nicogibert4-cyber <nicogibert4@gmail.com>
2026-09-15 22:43:19 -0300

    agregue nuevos ejercicios

--- Practica 1/ejercicio1.py
+++ Practica 1/ejercicio1.py
@@ -1,14 +1,14 @@
-# 1. Creamos una lista con números cualquiera
-numeros = [10, 15, 23, 30, 42, 50, 61, 100]
+# lista con números cualquiera
+lista_numeros = [10, 15, 23, 30, 42, 50, 61, 100]
 
-# 2. Le pedimos el múltiplo al usuario (convertido a entero con int)
+# Le pedimos el múltiplo al usuario (convertido a entero con int)
 multiplo = int(input("Ingresá un número para buscar sus múltiplos: "))
 
 print(f"Los múltiplos de {multiplo} en la lista son:")
 
-# 3. Recorremos la lista elemento por elemento
-for numero in numeros:
-    # Verificamos si NO es múltiplo (el resto de la división no es 0)
+# Recorremos la lista elemento por elemento
+for numero in lista_numeros:
+    # Verificamos si no es múltiplo (el resto de la división no es 0)
     if numero % multiplo != 0:
         continue  # Aborta esta iteración y salta al siguiente número de la lista
     
